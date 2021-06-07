35bed450fe1c0ea19586e33a6a7e5f7459880da3
Swmnx <No>
2021-06-07 23:27:58 +0300

    Initial Commit

---
+++ coreDataTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreDataTask
+//
+//  Created by swamnx on 7.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
